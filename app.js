3aeceb0d87b8d9b818670a15a240edbdfa00e975
NishithReddy <53988232+Nishith-Reddy@users.noreply.github.com>
2021-07-22 12:51:56 +0530

    made changes

--- app.js
+++ app.js
@@ -62,18 +62,34 @@ var scroll = new SmoothScroll('nav a[href*="#"]', {
 
 //nav highligh on scroll
 const navLit = () => {
-    const sections = document.querySelectorAll('section');
-    const links = document.querySelectorAll('.nav-links a');
-
-    sections.forEach(sections => {
-        sections.addEventListener('mouseover', function() {
-            const id = this.getAttribute('id');
-            const navActive = document.querySelector(`a[href="#${id}"]`);
-            links.forEach(link => link.classList.remove('active'));
-            navActive.classList.add('active');
-        })
-    })
-}
+    // $sections incleudes all of the container divs that relate to menu items.
+    var $sections = $('.container1');
+    // The user scrolls
+    $(window).scroll(function(){
+      // currentScroll is the number of pixels the window has been scrolled
+      var currentScroll = $(this).scrollTop();
+      // $currentSection is somewhere to place the section we must be looking at
+      var $currentSection;
+      // We check the position of each of the divs compared to the windows scroll positon
+      $sections.each(function(){
+        // divPosition is the position down the page in px of the current section we are testing      
+        var divPosition = $(this).offset().top;
+        console.log(divPosition);
+        // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+        // the -1 is so that it includes the div 1px before the div leave the top of the window.
+        if( divPosition - 150 < currentScroll ){
+          // We have either read the section or are currently reading the section so we'll call it our current section
+          $currentSection = $(this);
+          
+          // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+        }
+        // This is the bit of code that uses the currentSection as its source of ID
+        var id = $currentSection.attr('id');
+          $('.nav-links a').removeClass('active');
+          $("[href=#"+id+"]").addClass('active');
+      })
+    });
+  };
 
 //heading animation
 const text = document.querySelector(".heading");
@@ -133,7 +149,6 @@ function scrollDivAnimation(target) {
         div.classList.add('about-animation');
     }
     target = "";
-    console.log(divPosition);
 }
 
 function scrollImgAnimation(target) {
@@ -146,7 +161,6 @@ function scrollImgAnimation(target) {
         img.classList.add('img-animation');
     }
     target = "";
-    console.log(imgPosition);
 }
 
 window.addEventListener('scroll', function() {
