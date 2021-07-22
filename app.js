ed598c8d1660962e4778efd7507cdb5e33802a73
NishithReddy <53988232+Nishith-Reddy@users.noreply.github.com>
2021-07-22 12:04:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,81 +1,174 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nishith's Portfolio</title>
-    <link rel="stylesheet" href="fonts/Raleway-VariableFont_wght.ttf">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div>
-        <div class="preloader">
-            <p class="loading-img">N</p>
-        </div>
-        <nav class="navbar">    
-            <a href="" class="logo">N</a>    
-                <ul class="nav-links" id="navOptions">
-                    <li>
-                        <a href="#home" class="home-link">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about" class="about-link">About</a>
-                    </li>
-                    <li>
-                        <a href="#works">Works</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                </ul>
-                <div class="menu">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-        </nav>
-        <div class="container">
-            <section class="home" id="home">
-                <h1 class="heading">First Page</h1>
-                <p class="tag">Front-end Web Developer</p>
-                <div class="scroll">
-                    <a href="#about">
-                        <img src="/src/scroll.svg" alt="scroll animation">
-                    </a>
-                </div>
-            </section>
-            <section class="about" id="about">
-                <div class="about-me">
-                    <div class="about-section">
-                        <h2 class= 'intro-text1'>About Me</h2>
-                        <img src="/src/homepage.svg" alt="" class="about-img">
-                    </div>
-                    <div class="about-div">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi a saepe quo illum nisi ratione delectus. Neque dolorum eos vel dignissimos praesentium nemo alias corporis, amet recusandae ab ex rem.</p>
-                        <button>VIEW MY CV</button>
-                    </div>
-                </div>
-            </section>
-            <section class="works" id="works">
-                <h2 class="intro-text2">Third Page</h2>
-            </section>
-            <section class="contact" id="contact">
-                <h2 class="intro-text3">Forth Page</h2>
-            </section>
-        </div>       
-    </div>
-    <!-- <div class="home">
-        <h1>I'm Nishith Reddy</h1>
-        <p class="tag-line">Front-end Web Developer</p>
-        <div>
-            <img src="/src/homepage.svg" alt="svg image">
-        </div>
-    </div>
-    <dir>
-        <img src="/src/scroll.svg" alt="scroll animation">
-    </dir> -->
-    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
-    <script src="/app.js"></script>
-</body>
-</html>
+
+//preloader
+window.addEventListener('load', () => {
+        const preloader = document.querySelector('.preloader');
+        preloader.classList.add('preloader-finish')
+});
+//navbar mobile 
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    menu.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease fordwards ${index / 7 + 2}s`;
+                
+            }
+        }); 
+    });
+   
+}
+
+//smooth scroll 
+
+var scroll = new SmoothScroll('nav a[href*="#"]', {
+    speed:800,
+});
+
+// function smoothScroll(target,duration) {
+//     var target = document.querySelector(target);
+//     var targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+//     var startPosition = window.pageXOffset;
+//     var distance = targetPosition - startPosition 
+//     var startTime = null;
+
+//     function animation(currentTime) {
+//         if (startTime === null) {
+//             startTime = currentTime;
+//         }  
+//         var timeElapsed = currentTime - startTime;
+//         var run = ease(timeElapsed, startPosition, distance, duration);
+//         window.scrollTo(0,run);
+//         if (timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+
+//     function ease(t, b, c, d) {
+//         t /= d;
+//         return -c * t*(t-2) + b;
+//     }
+
+//     requestAnimationFrame(animation);
+
+
+//     console.log(targetPosition);
+// }
+
+
+
+//nav highligh on scroll
+const navLit = () => {
+    const sections = document.querySelectorAll('section');
+    const links = document.querySelectorAll('.nav-links a');
+
+    sections.forEach(sections => {
+        sections.addEventListener('mouseover', function() {
+            const id = this.getAttribute('id');
+            const navActive = document.querySelector(`a[href="#${id}"]`);
+            links.forEach(link => link.classList.remove('active'));
+            navActive.classList.add('active');
+        })
+    })
+}
+
+//heading animation
+const text = document.querySelector(".heading");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for(let i=0; i < splitText.length; i++) {
+    if(splitText[i] === " ") {
+        text.innerHTML +="<span>"+ "&nbsp;" + "</span>";
+    }
+    else {
+        text.innerHTML +="<span>"+splitText[i] + "</span>";
+
+    }
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
+//animation on scroll 
+
+function scrollAnimation(target) {
+    var introText = document.querySelector(target);
+    var introPosition = introText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.2;
+
+    introText.classList.remove('intro-animation');
+    if(Math.abs(introPosition) < screenPosition){
+        introText.classList.add('intro-animation');
+    }
+    target = "";
+}
+
+function scrollDivAnimation(target) {
+    var div = document.querySelector(target);
+    var divPosition = div.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.2;
+
+    div.classList.remove('about-animation');
+    if(Math.abs(divPosition) < screenPosition){
+        div.classList.add('about-animation');
+    }
+    target = "";
+    console.log(divPosition);
+}
+
+function scrollImgAnimation(target) {
+    var img = document.querySelector(target);
+    var imgPosition = img.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.2;
+
+    img.classList.remove('img-animation');
+    if(Math.abs(imgPosition) < screenPosition){
+        img.classList.add('img-animation');
+    }
+    target = "";
+    console.log(imgPosition);
+}
+
+window.addEventListener('scroll', function() {
+    scrollAnimation(".intro-text1");
+    scrollDivAnimation(".about-div");
+    scrollImgAnimation(".about-img")
+});
+window.addEventListener('scroll', function() {
+    scrollAnimation(".intro-text2");
+});
+window.addEventListener('scroll', function() {
+    scrollAnimation(".intro-text3");
+});
+navSlide();
+navLit();
+// var aboutLink = document.querySelector('.about-link');
+// var homeLink = document.querySelector('.home-link');
+// aboutLink.addEventListener('click', function() {
+//     smoothScroll('.about', 1000);
+
+// });
+// homeLink.addEventListener('click', function() {
+//     smoothScroll('.home', 1000);
+
+// });
